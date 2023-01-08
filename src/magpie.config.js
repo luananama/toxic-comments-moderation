755551271a473cc31c7cdd7e06785c690f6019dc
luananama <luanamariav@gmail.com>
2023-01-08 13:47:54 +0100

    remove commented out code

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -7,15 +7,3 @@ export default {
   mode: 'directLink',
   language: 'en'
 };
-
-
-// export default {
-//   experimentId: '86',
-//   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie',
-//   socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
-//   // this will be used in prolific mode
-//   completionUrl: 'https://app.prolific.co/submissions/complete?cc=1C9A8D5A',
-//   contactEmail: 'mchfranke@gmail.com',
-//   // Either 'debug', 'directLink' or 'prolific'
-//   mode: 'prolific'
-// };
